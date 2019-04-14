fb1150f9cfbfd7c219fe6de47e7f316262c3e1e4
Hytham <hytham.a@gmail.com>
2019-04-14 11:44:29 -0700

    Multi File to Multi host upload
    Added ability to allow for multiple files upload to a single host or multiple hosts.

--- Python/upload_file.py
+++ Python/upload_file.py
@@ -2,8 +2,18 @@
 
 import paramiko, getpass
 
-local = str(raw_input("Enter name of file to be uploaded: "))
-remote_file = str(raw_input("Remote filename: "))
+file_list = []
+entries = int(raw_input("How many files would you like to upload? "))
+
+for i in range(entries):
+	local_file = str(raw_input("Enter the name of the file to be uploaded: "))
+	remote_file = str(raw_input("Remote filename: "))
+
+	file_list.append({
+		"loc": local_file,
+		"rem": remote_file
+		})
+
 input = str(raw_input("Enter a switch, or switches, to upload to separated by a comma(,): "))
 host = input.split(",")
 port = int(raw_input("SSH listening port? "))
@@ -11,11 +21,15 @@ port = int(raw_input("SSH listening port? "))
 user = str(raw_input("Username: "))
 passwd = getpass.getpass()
 
-for i in host:
-	transport = paramiko.Transport((i, port))
-	transport.connect(username = user, password = passwd)
-	sftp = paramiko.SFTPClient.from_transport(transport)
+for a in file_list:
+	lf = a['loc']
+	rf = a['rem']
 
-	sftp.put(local,"/mnt/flash/" + remote_file)
-	sftp.close()
-	transport.close()
+	for b in host:
+		transport = paramiko.Transport((b, port))
+		transport.connect(username = user, password = passwd)
+		sftp = paramiko.SFTPClient.from_transport(transport)
+
+		sftp.put(lf,"/mnt/flash/" + rf)
+		sftp.close()
+		transport.close()
