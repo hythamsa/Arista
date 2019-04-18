a02b7b0f8f34f9c2d9f389c964a771cf60fa3783
Hytham <hytham.a@gmail.com>
2019-04-18 14:13:53 -0700

    Update get_inventory.py

--- CVP/get_inventory.py
+++ CVP/get_inventory.py
@@ -33,8 +33,8 @@ def main():
 	
 	inventory = (baseuri + "/inventory/devices")
 	get_inventory = requests.get(inventory, cookies=cookies, verify=False)
-	#get_inventory_json = get_inventory.json()
-	#pprint(get_inventory_json)
+	get_inventory_json = get_inventory.json()
+	pprint(get_inventory_json)
 
 if __name__ == '__main__':
 	main()
