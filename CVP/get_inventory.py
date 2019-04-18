1e9adf476d9854445d2950aba86de0c65dad5abc
Hytham <hytham.a@gmail.com>
2019-04-18 14:12:36 -0700

    .

--- CVP/get_inventory.py
+++ CVP/get_inventory.py
@@ -1,3 +1,6 @@
+
+#!/usr/local/bin/python3
+
 import requests, json, csv, argparse, sys
 from pprint import pprint
 
@@ -7,9 +10,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-u', '--user', dest='user', required='True', help='Username for switch auth')
 	parser.add_argument('-p', '--passwd', dest='passwd', required='True', help='Password for switch auth')
-	parser.add_argument('-s', '--switch', dest='switch', help='Provide a switch or a list of switches separated by a comma (,). Name or IP address are accepted')
-	parser.add_argument('-c', '--csvinfile', dest='csvinfile', help='CSV Input File')
-	parser.add_argument('-sr', '--server', dest='server', required='True', help='IP adddress or hostname of the CVP server')
+	parser.add_argument('-s', '--server', dest='server', required='True', help='IP adddress or hostname of the CVP server')
 	args = parser.parse_args()
 
 	# If no argument is supplied, print help
@@ -19,14 +20,9 @@ def main():
 
 	user = args.user
 	passwd = args.passwd
-	csvinfile = args.csvinfile
-	switch_list = args.switch
-	if not switch_list == None:
-		switch = switch_list.split(",")
 	server = args.server
 
 
-	hosts = json.dumps({'hosts': switch})
 	baseuri = "https://{}/cvpservice".format(server)
 
 	auth_data = json.dumps({'userId': user, 'password': passwd})
@@ -36,10 +32,9 @@ def main():
 
 	
 	inventory = (baseuri + "/inventory/devices")
-	post_inventory = requests.post(inventory,cookies=cookies,data=hosts,verify=False)
-	print(post_inventory.status_code)
-	print(post_inventory.content)
-	
+	get_inventory = requests.get(inventory, cookies=cookies, verify=False)
+	#get_inventory_json = get_inventory.json()
+	#pprint(get_inventory_json)
 
 if __name__ == '__main__':
 	main()
