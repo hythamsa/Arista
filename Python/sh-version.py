775f939ba4ce393f51ab8a1c600fa3f1e47d066e
Hytham <hytham.a@gmail.com>
2019-04-23 16:27:31 -0700

    Class colour definition
    class color:
        HEADER = '\033[95m'
        PURPLE = '\033[95m'
        CYAN = '\033[96m'
        DARKCYAN = '\033[36m'
        BLUE = '\033[94m'
        GREEN = '\033[92m'
        YELLOW = '\033[93m'
        RED = '\033[91m'
        BOLD = '\033[1m'
        UNDERLINE = '\033[4m'
        END = '\033[0m'

--- Python/sh-version.py
+++ Python/sh-version.py
@@ -3,6 +3,19 @@
 import sys, getpass, datetime, csv, ssl
 from jsonrpclib import Server
 
+class color:
+    HEADER = '\033[95m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
 
 def main():
     while True:
@@ -88,11 +101,13 @@ def term():
                 swver = getver[0]["version"]
                 uptime = getver[0]["uptime"]
 
-                print '#' * 8, "Version Data for %s" %a, '#' * 12
-                print "\tSerial Number: \t%s" % ser
-                print "\tModel Number: \t%s" % model
-                print "\tSW Version: \t%s" % swver
-                print "\tUp Time: \t%s" % uptime
+                print('')
+                print('#' * 8 + color.BOLD + '\tVersion Data for {}\t    '.format(a) + color.END + '#' * 12)
+                print("\t  Serial Number: {}".format(ser))
+                print("\t  Model Number: {}".format(model))
+                print("\t  SW Version: {}".format(swver))
+                print("\t  Up Time: {}".format(uptime))
+                print('')
     except:
         sys.exit(2)
 
